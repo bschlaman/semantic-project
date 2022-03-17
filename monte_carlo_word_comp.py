5316fc57ec1ac036cd8cb7511a181c186f4708a0
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-03-17 21:00:55 +0000

    changing datastructure of the semantic graph

--- monte_carlo_word_comp.py
+++ monte_carlo_word_comp.py
@@ -5,6 +5,7 @@
 # semantic transitivity (e.g. if A-B = 1 and B-C = 1, A-C = 1)
 # aim to reduce the manual comparisons down from (n^2)/2
 import itertools
+import collections
 import random
 from pprint import pprint
 
@@ -13,6 +14,7 @@ random.seed(9)
 
 # semantic values; sd[(w1, w2)] = 0 - 4
 sd = {}
+sem_graph = collections.defaultdict(list)
 
 def import_wordlist():
     with open(WORD_FILE, "r") as f:
@@ -23,21 +25,25 @@ def assign_random_val():
     return random.randrange(0,5)
 
 def apply_rule(w1, w2):
-    # A-C is 0 if A-B is 4 and B-C is 0
-    for pair in sd:
-        if w1 not in pair and w2 not in pair:
-            continue
-        (sdw1, sdw2) = pair
-        if w1 == sdw1:
-            pass
+    # A-C is 0 if there exists B s.t. A-B is 4 and B-C is 0
+    # A-C is 0 if there exists B s.t. A-B is 0 and B-C is 4
+    for B, sval in sem_graph[w1]:
+        if B not in sem_graph: continue
+        if sval == 4 and (w2, 0) in sem_graph[B]:
+            return 0
+        if sval == 0 and (w2, 4) in sem_graph[B]:
+            return 0
     return -1
 
 def main():
     words = import_wordlist()
     random.shuffle(words)
-    words = words[:7]
+    words = words[:111]
+    # w1 must come before w2 lexicographically
+    words.sort()
     pairs = list(itertools.combinations(words, 2))
-    print(words, len(words))
+    # if pairs are sorted, B will never be found in sem_graph
+    random.shuffle(pairs)
 
     manual_comp_count = 0
     for (w1, w2) in pairs:
@@ -45,13 +51,12 @@ def main():
         if res == -1:
             manual_comp_count += 1
             res = assign_random_val()
-        sd[(w1, w2)] = res
-    pprint(sd)
-    k = list(sd.keys())
-    random.shuffle(k)
-    for w in [" ".join(i) for i in k]:
-        print(w)
+        sem_graph[w1].append((w2, res))
+
+    print(f"number of words:    {len(words)}")
+    print(f"number of pairs:    {len(pairs)}")
     print(f"manual comparisons: {manual_comp_count}")
+    print(f"total comparisons:  {len(pairs)}")
 
 
 if __name__ == "__main__":
