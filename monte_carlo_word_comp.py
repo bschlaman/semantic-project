787fec8a335b1808b9ea2b94d985ace6157a9a97
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-03-17 11:17:16 -0400

    adding more logic

--- monte_carlo_word_comp.py
+++ monte_carlo_word_comp.py
@@ -6,22 +6,52 @@
 # aim to reduce the manual comparisons down from (n^2)/2
 import itertools
 import random
+from pprint import pprint
 
 WORD_FILE = "words.txt"
-random.seed(10)
+random.seed(9)
+
+# semantic values; sd[(w1, w2)] = 0 - 4
+sd = {}
 
 def import_wordlist():
     with open(WORD_FILE, "r") as f:
         words = f.read().splitlines()
     return words
 
+def assign_random_val():
+    return random.randrange(0,5)
+
+def apply_rule(w1, w2):
+    # A-C is 0 if A-B is 4 and B-C is 0
+    for pair in sd:
+        if w1 not in pair and w2 not in pair:
+            continue
+        (sdw1, sdw2) = pair
+        if w1 == sdw1:
+            pass
+    return -1
+
 def main():
     words = import_wordlist()
-    words = words[60:65]
+    random.shuffle(words)
+    words = words[:7]
     pairs = list(itertools.combinations(words, 2))
-    print(pairs[:4])
-    print(type(pairs[3]))
-    print(random.randint(0,5))
+    print(words, len(words))
+
+    manual_comp_count = 0
+    for (w1, w2) in pairs:
+        res = apply_rule(w1, w2)
+        if res == -1:
+            manual_comp_count += 1
+            res = assign_random_val()
+        sd[(w1, w2)] = res
+    pprint(sd)
+    k = list(sd.keys())
+    random.shuffle(k)
+    for w in [" ".join(i) for i in k]:
+        print(w)
+    print(f"manual comparisons: {manual_comp_count}")
 
 
 if __name__ == "__main__":
