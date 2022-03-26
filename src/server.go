8ccf75c78841c391a9f4180d3240cae6b4b53b83
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-03-26 15:39:26 -0400

    improvments to server logging

--- src/server.go
+++ src/server.go
@@ -1,37 +1,36 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"os"
 
-	log "github.com/bschlaman/b-utils/pkg/logger"
+	"github.com/bschlaman/b-utils/pkg/logger"
 	"github.com/bschlaman/b-utils/pkg/utils"
 )
 
 const (
 	serverName string = "SEMANTIC-SERVER"
-	port       string = ":8080"
+	port       string = ":8081"
 	logPath    string = "logs/output.log"
 	configPath string = "config.json"
 	staticDir  string = "assets/static"
 )
 
-var logger *log.BLogger
+var log *logger.BLogger
 
 func init() {
-	// Print something to stdout for testing purposes
-	println(" ### Starting", serverName, "on port", port)
-
 	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
-	logger.Info(" ### Starting", serverName, "on port", port)
+	mw := io.MultiWriter(file, os.Stdout)
+	log = logger.New(mw)
 }
 
 func main() {
-	http.Handle("/echo", utils.LogReq(logger)(utils.EchoHandle()))
-	// http.Handle("/get_words", utils.LogReq(logger)(getWordsHandle()))
-	logger.Info("starting http server on port", port)
-	logger.Fatal(http.ListenAndServe(port, nil))
+	http.Handle("/echo", utils.LogReq(log)(utils.EchoHandle()))
+	// http.Handle("/get_words", utils.LogReq(log)(getWordsHandle()))
+	log.Info("starting http server on port", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
