e8ec353bfe5f3e4770988c5e8efc4b38f37702cc
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-03-29 08:58:38 -0400

    adding basic db connection logic to api

--- src/server.go
+++ src/server.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 
 	"github.com/bschlaman/b-utils/pkg/logger"
 	"github.com/bschlaman/b-utils/pkg/utils"
+	"github.com/jackc/pgx/v4"
 )
 
 const (
@@ -19,6 +22,26 @@ const (
 
 var log *logger.BLogger
 
+func getWordsHandle() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+			os.Exit(1)
+		}
+		defer conn.Close(context.Background())
+
+		var id, word1, word2 string
+		err = conn.QueryRow(context.Background(), "select id, word1, word2 from words limit 1").Scan(&id, &word1, &word2)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Fprintln(w, id, word1, word2)
+	})
+}
+
 func init() {
 	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
@@ -30,7 +53,7 @@ func init() {
 
 func main() {
 	http.Handle("/echo", utils.LogReq(log)(utils.EchoHandle()))
-	// http.Handle("/get_words", utils.LogReq(log)(getWordsHandle()))
+	http.Handle("/get_words", utils.LogReq(log)(getWordsHandle()))
 	log.Info("starting http server on port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
