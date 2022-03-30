5be745307143dde8835515ac3bbd4f050fe943ed
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-03-29 22:47:52 -0400

    fixing type name in table create

--- setup.sql
+++ setup.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS public.words
     word1 character varying(24) NOT NULL,
     word2 character varying(24) NOT NULL,
     sem_similarity smallint CHECK (sem_similarity >= 0 AND sem_similarity <= 4),
-    status pair_status DEFAULT 'PENDING'::word_pair_status,
+    status word_pair_status DEFAULT 'PENDING'::word_pair_status,
     CONSTRAINT lex_order_check CHECK (word1::text < word2::text)
 );
 ALTER TABLE words ADD CONSTRAINT unique_word_pair UNIQUE(word1, word2);
